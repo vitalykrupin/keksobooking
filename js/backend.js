a0602ca22172dd4eb8b45a17cf4ede22d09b0cd1
Vitaly <vitalykrupin@gmail.com>
2018-07-03 13:41:22 +0500

    backend module was fixed

--- js/backend.js
+++ js/backend.js
@@ -1,39 +1,17 @@
 'use strict';
 
 (function () {
+  var TIMEOUT = 10000;
+  var STATUS_SUCCESS = 200;
+
   window.backend = {
-    load: function (onLoad, onError) {
-      var URL = 'https://js.dump.academy/keksobooking/data';
+    request: function (url, type, onLoad, onError, data) {
+      var URL = url;
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 10000;
-
-      xhr.open('GET', URL);
-      xhr.send();
-    },
-
-    upload: function (data, onLoad, onError) {
-      var URL = 'https://js.dump.academy/keksobooking';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
+        if (xhr.status === STATUS_SUCCESS) {
           onLoad(xhr.response);
         } else {
           onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
@@ -48,9 +26,9 @@
         onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
       });
 
-      xhr.timeout = 10000;
+      xhr.timeout = TIMEOUT;
 
-      xhr.open('POST', URL);
+      xhr.open(type, URL);
       xhr.send(data);
     }
   };
