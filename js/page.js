2a50a107ebd21731a7fc07ddd5f7adb3578bf470
Vitaly <vitalykrupin@gmail.com>
2018-07-03 23:52:36 +0500

    activate/deactivate methods were rebuild

--- js/page.js
+++ js/page.js
@@ -2,11 +2,9 @@
 
 (function () {
   var selects = document.querySelectorAll('select');
-  var map = document.querySelector('.map');
   var fieldset = document.querySelectorAll('fieldset');
-  // var mapPinMain = map.querySelector('.map__pin--main');
-  // var adForm = document.querySelector('.ad-form');
-  // var resetButton = adForm.querySelector('.ad-form__reset');
+  var map = document.querySelector('.map');
+  var mapPinMain = map.querySelector('.map__pin--main');
 
   window.page = {
     activate: function () {
@@ -15,27 +13,26 @@
       for (var i = 0; i < selects.length; i++) {
         selects[i].disabled = false;
       }
-
       for (i = 0; i < fieldset.length; i++) {
         fieldset[i].disabled = false;
       }
-
-      window.setAddressValues();
-      window.pin.createFragmentPins(window.data.OFFERS);
-      // mapPinMain.removeEventListener('mousedown', window.page.activate);
+      window.form.setAddressValues();
+      window.pin.createPins(window.data.OFFERS);
+      mapPinMain.removeEventListener('mousedown', window.page.activate);
     },
     deactivate: function () {
+      window.form.resetAdForm();
       map.classList.add('map--faded');
-      window.pin.deleteFragmentPins();
-      window.card.deleteCard();
       document.querySelector('.ad-form').classList.add('ad-form--disabled');
       for (var i = 0; i < selects.length; i++) {
         selects[i].disabled = true;
       }
-      document.querySelector('fieldset').disabled = true;
-
-      // resetButton.removeEventListener('click', window.page.deactivate);
-      // mapPinMain.addEventListener('mousedown', window.page.activate);
+      for (i = 0; i < fieldset.length; i++) {
+        fieldset[i].disabled = true;
+      }
+      window.card.closePopup(); // to make this method
+      window.pin.removePins(); // to make this method
+      window.pin.returnMainPin(); // to make this method
     }
   };
 })();
