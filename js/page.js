c5f85fbbac689ca00cce464ef19f03faeaed63dc
Vitaly <vitalykrupin@gmail.com>
2018-07-03 17:21:05 +0500

    page deactivation back

--- js/page.js
+++ js/page.js
@@ -24,15 +24,15 @@
       window.pin.createFragmentPins(window.data.OFFERS);
       // mapPinMain.removeEventListener('mousedown', window.page.activate);
     },
-    // deactivate: function () {
-    //   map.classList.add('map--faded');
-    //   window.pin.deleteFragmentPins();
-    //   window.card.deleteCard();
-    //   document.querySelector('.ad-form').classList.add('ad-form--disabled');
-    //   for (var i = 0; i < selects.length; i++) {
-    //     selects[i].disabled = true;
-    //   }
-    //   document.querySelector('fieldset').disabled = true;
+    deactivate: function () {
+      map.classList.add('map--faded');
+      window.pin.deleteFragmentPins();
+      window.card.deleteCard();
+      document.querySelector('.ad-form').classList.add('ad-form--disabled');
+      for (var i = 0; i < selects.length; i++) {
+        selects[i].disabled = true;
+      }
+      document.querySelector('fieldset').disabled = true;
 
       // resetButton.removeEventListener('click', window.page.deactivate);
       // mapPinMain.addEventListener('mousedown', window.page.activate);
