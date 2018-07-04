1090cd59d35c0f0c91b30b00bcc33937bee38856
Vitaly <vitalykrupin@gmail.com>
2018-07-04 13:23:16 +0500

    some changes

--- js/page.js
+++ js/page.js
@@ -5,11 +5,12 @@
   var fieldset = document.querySelectorAll('fieldset');
   var map = document.querySelector('.map');
   var mapPinMain = map.querySelector('.map__pin--main');
+  var adForm = document.querySelector('.ad-form');
 
   window.page = {
     activate: function () {
       map.classList.remove('map--faded');
-      document.querySelector('.ad-form').classList.remove('ad-form--disabled');
+      adForm.classList.remove('ad-form--disabled');
       for (var i = 0; i < selects.length; i++) {
         selects[i].disabled = false;
       }
@@ -17,13 +18,24 @@
         fieldset[i].disabled = false;
       }
       window.form.setAddressValues();
-      window.pin.createPins(window.data.OFFERS);
+      // window.pin.createPins(window.data.OFFERS);
+      window.backend.request('https://js.dump.academy/keksobooking/data', 'GET', function (response) {
+        response.forEach(function (offer, index) {
+          offer.index = index;
+        });
+        window.data = {
+          OFFERS: response
+        };
+        window.pin.createPins(window.data.OFFERS);
+      });
+
       mapPinMain.removeEventListener('mousedown', window.page.activate);
     },
     deactivate: function () {
       window.form.resetAdForm();
-      map.classList.add('map--faded');
-      document.querySelector('.ad-form').classList.add('ad-form--disabled');
+      adForm.classList.add('ad-form--disabled');
+      // map.classList.add('map--faded');
+      // document.querySelector('.ad-form').classList.add('ad-form--disabled');
       for (var i = 0; i < selects.length; i++) {
         selects[i].disabled = true;
       }
@@ -33,6 +45,12 @@
       window.card.closePopup(); // to make this method
       window.pin.removePins();
       window.pin.returnMainPin();
+      map.classList.add('map--faded');
+      window.form.setAddressValues();
+
+      var adFormReset = adForm.querySelector('.ad-form__reset');
+      adFormReset.removeEventListener('click', window.page.deactivate);
+      mapPinMain.addEventListener('mousedown', window.page.activate);
     }
   };
 })();
