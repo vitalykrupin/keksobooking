f01cc0654d02a1f542ec035f9939f3038a351298
Vitaly <vitalykrupin@gmail.com>
2018-07-05 18:24:39 +0500

    form reset fixed

--- js/filters.js
+++ js/filters.js
@@ -52,7 +52,6 @@
   var updatePins = function (offers) {
     featuresFilterElements = mapFiltersElement.querySelectorAll('input[type=checkbox]:checked');
 
-
     var filteredOffers = [];
     for (var i = 0; i < offers.length; i++) {
       if (checkSelects(offers[i]) && checkFeatures(offers[i])) {
